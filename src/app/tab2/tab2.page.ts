afee582818293eefffa80e1ed10097f2e530f6d1
Adriano Evens <jukapp22@gmail.com>
2025-03-28 21:22:00 -0300

    first commit

---
+++ src/app/tab2/tab2.page.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tab2',
+  templateUrl: 'tab2.page.html',
+  styleUrls: ['tab2.page.scss'],
+  standalone: false,
+})
+export class Tab2Page {
+
+  constructor() {}
+
+  cadastrar(){
+    alert('Usuário Cadastrado');
+  }
+
+}
---
+++ src/app/tab3/tab3.page.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tab3',
+  templateUrl: 'tab3.page.html',
+  styleUrls: ['tab3.page.scss'],
+  standalone: false,
+})
+export class Tab3Page {
+
+  constructor() {}
+  Acessar(){
+    alert('Usuário Logado');
+  }
+
+}
+
